3878743e81b5e8b7e0907419a2c695890ae447af
Christian Fobel <christian@fobel.net>
2017-10-04 16:38:14 -0400

    refactor(util): refactor surface->array into to_array

--- src/docket/util.py
+++ src/docket/util.py
@@ -1,10 +1,32 @@
 # coding: utf-8
-import matplotlib as mpl
-import matplotlib.pyplot
+import io
+import platform
+
+from PIL import Image
 import numpy as np
 
 
 __all__ = ['plot_surface']
+
+
+def to_array(surface):
+    '''
+    Convert Pango image surface to numpy array.
+
+    Parameters
+    ----------
+    surface : pango.ImageSurface
+        Pango image surface.
+
+    Returns
+    -------
+    numpy.array
+        Image RGB array.
+    '''
+    dims = {key_i: getattr(surface, 'get_%s' % key_i)()
+            for key_i in ('width', 'height', 'stride')}
+    return (np.fromstring(surface.get_data(), dtype='uint8')
+            .reshape(dims['height'], dims['width'], -1)[:, :, 2::-1])
 
 
 def plot_surface(surface, axis=None):
@@ -21,11 +43,10 @@ def plot_surface(surface, axis=None):
     matplotlib.axes._subplots.AxesSubplot
         Matplotlib axis containing drawn image.
     '''
-    dims = {key_i: getattr(surface, 'get_%s' % key_i)()
-            for key_i in ('width', 'height', 'stride')}
-    data = (np.fromstring(surface.get_data(), dtype='uint8')
-            .reshape(dims['height'], dims['width'], -1)[:, :, 2::-1])
+    import matplotlib as mpl
+    import matplotlib.pyplot
 
+    data = to_array(surface)
     if axis is None:
         fig, axis = mpl.pyplot.subplots()
     axis.imshow(data)
